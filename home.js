c0090266e48b484bdb65ae400b44c08fdd32a281
Henry Zhang <sirhenryzhang@gmail.com>
2023-04-28 21:42:03 -0400

    polish

--- home.js
+++ home.js
@@ -7,6 +7,6 @@ function homeDisplay(){
     textSize(20);
     text("Welcome!",500,200);
     textAlign(LEFT,TOP);
-    text("My name is Henry Zhang, and this is my portfolio.\n\nI am an aspiring game developer, and I use C++, JavaScript, C#, and Java.\n\nIn my free time, I play the tenor saxophone, and play ping pong, soccer, and badminton.",500,300);
+    text("My name is Henry Zhang, and this is my portfolio.\n\nI am an aspiring game developer, and I use C++, JavaScript, C#, and Java.\n\nIn my free time, I play the tenor saxophone, ping pong, soccer, and badminton.",500,300);
     text("What you saw on the landing screen was an implementation of John Conway's Game of Life,\none of my many projects and sketches.",500,500);
 }
